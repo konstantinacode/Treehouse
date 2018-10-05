f134346b5cc47ede1068fd56f332fb5eb380b6b5
Konstantina Matthaiou <konstantina_mat@hotmail.com>
2018-10-06 00:35:18 +0300

    Capture Input
    Capturing Visitor Input
    Combining Strings

---
+++ jsInput/scripts.js
@@ -0,0 +1,6 @@
+var visitor = prompt('What is your name?');
+var message = 'Hello ' + visitor + ". Welcome to Treehouse. ";
+message = message + "We are so glad that you come by to visit, ";
+message += visitor;
+message += ". Please come again, when you want to learn some more.";
+document.write(message);
