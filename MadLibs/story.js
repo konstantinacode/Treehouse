d53c3b19356b0249e41b13e68547056ee386b91b
Konstantina Matthaiou <konstantina_mat@hotmail.com>
2018-10-06 01:06:18 +0300

    Mad Libs Game

---
+++ MadLibs/story.js
@@ -0,0 +1,7 @@
+var adjective = prompt("Please type a adjective");
+var verb = prompt("Please type a verb");
+var noun = prompt("Please type a noun");
+
+alert("All done. Ready for the message?");
+
+document.write("<h2>There once was a " + adjective + " programmer who wanted to use JavaScript to " + verb + " the " + noun + ".</h2>");
