5535b4fec4e05c9fd837ea5b89b88a835b792926
Konstantina Matthaiou <konstantina_mat@hotmail.com>
2018-10-11 22:40:08 +0300

    Parse Strings to Numbers

---
+++ ParseStringsToNumbers/scripts.js
@@ -0,0 +1,6 @@
+var HTMLBadges = prompt('How many HTML badges do you have?');
+var CSSBadges = prompt('How many CSS badges do you have?');
+var totalBadges = parseInt(HTMLBadges) + parseInt(CSSBadges);
+alert('Wow! You have ' + totalBadges + ' badges!');
+
+//there is also parseFloat(...);
