a54f6650a65b74a433d69744e564370e96718b59
Konstantina Matthaiou <konstantina_mat@hotmail.com>
2018-10-11 22:39:38 +0300

    Time Calculator

---
+++ TimeCalculator/scripts.js
@@ -0,0 +1,10 @@
+var secondsPerMin = 60;
+var minsPerHour = 60;
+var hoursPerDay = 24;
+var daysPerWeek = 7;
+var weeksPerYear = 52;
+var secondsPerDay = secondsPerMin * minsPerHour * hoursPerDay;
+document.write('<p>There are ' + secondsPerDay + ' seconds in a day</p>');
+var yearsAlive = 27;
+var secAlive = secondsPerDay * daysPerWeek * weeksPerYear * yearsAlive;
+document.write("<p>I've been alive for more than " + secAlive + " seconds.</p>" );
