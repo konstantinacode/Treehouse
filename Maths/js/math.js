bc3309ff7cd8326f14ac785253c262ce75e228e8
Konstantina Matthaiou <konstantina_mat@hotmail.com>
2018-10-12 18:59:30 +0300

    with checks
    checks if it has been given a NaN or the second number is 0

--- Maths/js/math.js
+++ Maths/js/math.js
@@ -1,56 +1,31 @@
-// 1. Attach this file -- math.js -- to the index.html file using a <script> tag
-
-// 2. Add an alert to announce the program with a message like "Let's do some math!"
+// declare program variables
+var num1;
+var num2;
+var message;
+// announce the program
 alert("Let's do some math!");
 
-// 3. Create a variable and use the prompt() method to collect a number from a visitor
-var fNum = prompt("Please type a number");
+// collect numeric input
+num1 = prompt("Please type a number");
+num1 = parseFloat(num1);
+num2 = prompt("Please type another number");
+num2 = parseFloat(num2);
 
-// 4. Convert that value from a string to a floating point number
-fNum = parseFloat(fNum);
+if ( num2 === 0 ){
+  alert("The second number is 0. You can't divide by zero. Reload and try again.");
+} else if ( isNaN(num1) || isNaN(num2) ){
+  alert("At least one of the values you typed is not a number. Reload and try again.");
+} else {
+  //build an html message
+  message = "<h1>Math with the numbers " + num1 + " and " + num2 + "  </h1>";
+  message += num1 + " + " + num2 + " = " + (num1 + num2);
+  message += "<br>";
+  message += num1 + " * " + num2 + " = " + (num1 * num2);
+  message += "<br>";
+  message += num1 + " / " + num2 + " = " + (num1 / num2);
+  message += "<br>";
+  message += num1 + " - " + num2 + " = " + (num1 - num2);
 
-// 5. Repeat steps 3 and 4 to create a second variable and collect a second number
-var sNum = prompt("Please type another number");
-sNum = parseFloat(sNum);
-
-// 6. Create a new variable -- message -- which you'll use to build
-//    a complete message to print to the document
-//    Start by creating a string that includes <h1> tags as well
-//    and the two input numbers. The string should look something like this:
-//    "<h1>Math  with the numbers 3 and 4</h1>" where the two numbers are
-//    the values input from the user. Use string concatenation to create this
-//    and make sure you actually perform the math on the values by 
-//    using the + symbol to add their values together
-var message = "<h1>Math  with the numbers " + fNum + " and " + sNum + "</h1>";
-// 7. Add another string to the message variable. 
-//    The string should look something like this after concatenation:
-//    "3 + 4 = 7"
-message += "<p>" + fNum + " + " + sNum + " = " + (fNum + sNum);
-
-// 8. Add a linebreak tag -- <br> -- to the message variable
-message += "<br>";
-// 9. Continue to add to the message variable to include strings
-//    demonstrating multiplication, division and subtraction
-//    For example:
-//    "3 * 4 = 12"
-//    "3 / 4 = 0.75"
-//    "3 - 4 = -1"
-message += fNum + " * " + sNum + " = " + (fNum * sNum) + "<br>";
-message += fNum + " / " + sNum + " = " + (fNum / sNum) + "<br>";
-message += fNum + " - " + sNum + " = " + (fNum - sNum) + "</p>";
-
-
-// 10. Use the document.write() method to print the message variable 
-//     to the web page. Open the finished.png file in this workspace
-//     to see what the completed output should look like
-document.write(message);
-
-
-
-
-
-
-
-
-
-
+  // write message to web page
+  document.write(message);
+}
